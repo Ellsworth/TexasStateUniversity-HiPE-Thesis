a27a2d311ce324aeb8d7ba10135c039af8ad117e
Erich Ellsworth <erich@kg5key.com>
2025-11-27 22:02:12 -0600

    Remove unused files. Let Gazebo handle downloads.

--- unzip_files.sh
+++
@@ -1,8 +0,0 @@
-OWNER=local
-
-for z in assets/models_zip/*.zip; do
-    name=$(basename "$z" .zip)
-    dest=/home/erich/Software/TexasStateUniversity-HiPE-Thesis/assets/models/fuel.ignitionrobotics.org/$OWNER/models/"$name"/1
-    mkdir -p "$dest"
-    unzip -o "$z" -d "$dest"
-done
